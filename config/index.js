df947321d60d930243e53a449a78d40a369c7e8c
Tong Mu <dkwingsmt@gmail.com>
2016-08-10 23:46:35 +0800

    Change server

--- config/index.js
+++ config/index.js
@@ -26,7 +26,7 @@ const config = {
   // ----------------------------------
   // Server Configuration
   // ----------------------------------
-  server_host : process.env.LOCALHOST ? '127.0.0.1' : 'ec2-52-42-141-105.us-west-2.compute.amazonaws.com', // use string 'localhost' to prevent exposure on local network
+  server_host : process.env.LOCALHOST ? '127.0.0.1' : 'ec2-52-42-137-218.us-west-2.compute.amazonaws.com', // use string 'localhost' to prevent exposure on local network
   server_port : process.env.PORT || 3000,
 
   // ----------------------------------
